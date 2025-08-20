e24918131c93f1f311e68b214fca2452b3bd2d07
Dawid <dawid@linek.dev>
2025-08-20 22:54:28 +0200

    feat: normal experiment setup

--- example.py
+++ example.py
@@ -11,7 +11,7 @@ from src.data.DataPipeline import DataPipeline
 from src.evaluators.EvaluatorPipeline import EvaluatorPipeline
 
 if __name__ == "__main__":
-    pipeline = DataPipeline("2025-06-01","2025-08-20")
+    pipeline = DataPipeline("2023-07-01","2025-08-20")
     
     pipeline.add_source(EntsoeSource("PL","fade2e5f-6d62-4354-9f95-e8629acec0e9"))
     pipeline.add_source(OpenMeteoSource(52.2298,21.0118, columns=["temperature_2m", "precipitation","cloud_cover"], horizon=1))
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     
     dummies = ['is_monday', 'is_tuesday', 'is_wednesday', 'is_thursday', 'is_friday', 'is_saturday', 'is_sunday','is_holiday']
     
-    pipeline = EvaluatorPipeline(1, data, "2025-07-15", "2025-08-01")
+    pipeline = EvaluatorPipeline(1, data, "2024-08-01", "2025-08-01")
     
     pipeline.add_model(OLSModel(['load','load_d-1','load_d-7'], dummies, name="AR", target='load', trainingWindow=10, saveToFile="ar.json"))
     pipeline.add_model(LassoModel(['load','load_d-1','load_d-7','temperature_2m_d+1','cloud_cover_d+1','precipitation_d+1'], dummies, name="ARX", target='load', trainingWindow=10, saveToFile="arx.json"))
