62678c1e9bca540e8fe0151f6ea89d59ef955ef7
Dawid <dawid@linek.dev>
2025-08-26 19:21:18 +0200

    feat: csv source

---
+++ src/data/sources/CsvSource.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from entsoe import EntsoePandasClient
+from rich.progress import Progress
+
+class CsvSource():
+
+    def __init__(self, path: str, index: str):
+        self.path = path
+        self.index = index
+
+    def fetch(self, start_date, end_date) -> pd.DataFrame:
+        data = pd.read_csv(self.path)
+        data.index=pd.to_datetime(data[self.index])
+        return data[start_date:end_date,:]
