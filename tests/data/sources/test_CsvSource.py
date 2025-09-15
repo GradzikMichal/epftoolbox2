7d5f662175fcc5543411883e7c3d73d36f02254a
Michal Gradzik <65037147+GradzikMichal@users.noreply.github.com>
2025-09-15 15:06:27 +0200

    Changing requirements.txt because of github actions failed
    * Added testing to README.md, changed path to files and added coverage to requirements.txt

--- tests/data/sources/test_CsvSource.py
+++ tests/data/sources/test_CsvSource.py
@@ -3,6 +3,9 @@ import pydantic
 import pytest
 from src.data.sources.CsvSource import CsvSource
 
+file_path_good_file = "tests/files/test_data.csv"
+file_path_bad_file = "tests/files/test.csv"
+
 
 def test_csv_source_without_args():
     with pytest.raises(TypeError):
@@ -35,23 +38,21 @@ def test_csv_source_with_incorrect_required_args():
 
 
 def test_csv_source_fetch_existing_data():
-    csvSource = CsvSource(path="../../files/test_data.csv", index_col="datetime")
+    csvSource = CsvSource(path=file_path_good_file, index_col="datetime")
     data = csvSource.fetch_data()
     assert pd.DataFrame == type(data)
 
 
 def test_csv_source_fetch_non_existing_data(capfd):
-    file_path = "../../files/test.csv"
-    csvSource = CsvSource(path=file_path, index_col="datetime")
+    csvSource = CsvSource(path=file_path_bad_file, index_col="datetime")
     with pytest.raises(SystemExit):
         csvSource.fetch_data()
     out, err = capfd.readouterr()
-    assert out == f"[Errno 2] No such file or directory: '{file_path}'\n[red]Error while reading file! File not found[/red]\n"
+    assert out == f"[Errno 2] No such file or directory: '{file_path_bad_file}'\n[red]Error while reading file! File not found[/red]\n"
 
 
 def test_csv_source_fetch_data_within_date_range_and_date_and_index_col_are_the_same():
-    file_path = "../../files/test_data.csv"
-    csvSource = CsvSource(path=file_path, index_col="datetime", date_col="datetime")
+    csvSource = CsvSource(path=file_path_good_file, index_col="datetime", date_col="datetime")
     data = csvSource.fetch_data_within_date_range(start_date="2023-01-01 01:00:00", end_date="2023-01-01 03:00:00")
     assert pd.DataFrame == type(data)
     assert data.index[0] == pd.Timestamp("2023-01-01 01:00:00")
@@ -59,15 +60,14 @@ def test_csv_source_fetch_data_within_date_range_and_date_and_index_col_are_the_
 
 
 def test_csv_source_fetch_data_within_date_range_and_date_and_index_col_are_different():
-    file_path = "../../files/test_data.csv"
-    csvSource = CsvSource(path=file_path, index_col=None, date_col="datetime")
+    csvSource = CsvSource(path=file_path_good_file, index_col=None, date_col="datetime")
     data = csvSource.fetch_data_within_date_range(start_date="2023-01-01 01:00:00", end_date="2023-01-01 03:00:00")
     assert pd.DataFrame == type(data)
     assert data["datetime"][data.index[0]] == "2023-01-01 01:00:00"
     assert data["datetime"][data.index[-1]] == "2023-01-01 03:00:00"
 
+
 def test_csv_source_fetch_data_within_date_range_wrong_date_col():
-    file_path = "../../files/test_data.csv"
-    csvSource = CsvSource(path=file_path, index_col="datetime")
+    csvSource = CsvSource(path=file_path_good_file, index_col="datetime")
     with pytest.raises(KeyError):
         csvSource.fetch_data_within_date_range(start_date="2023-01-01 01:00:00", end_date="2023-01-01 03:00:00")
